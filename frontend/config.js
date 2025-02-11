2c6ed36239ab87fc66a81ad688571ca167a6dc2e
kevin <120391759+kevi07@users.noreply.github.com>
2025-02-11 14:59:23 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const api_url = 'http://localhost:3000/api/v1/';
-export const api_url = 'https://jobhunter-xohx.onrender.com';
+// export const api_url = 'http://localhost:3000/api/v1/'
+export const api_url = 'https://jobhunter-xohx.onrender.com'
